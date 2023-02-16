9601d659eb53d634d2ef79bef9ff8a36037bc962
jryano123 <johnreyano05@gmail.com>
2023-02-16 15:53:47 +0800

    Add project files.

---
+++ Yano ACt2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Please enter score #1");
+int score = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter score #2");
+score += Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter score #3");
+score += Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter score #4");
+score += Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter score #5");
+score += Convert.ToInt32(Console.ReadLine());
+
+var finalscore = score;
+
+Console.WriteLine("Your Average score is:" + finalscore);
+
+if(score >= 75)
+{
+    Console.WriteLine("Passed");
+}
+else
+{
+    Console.WriteLine("Failed");
+}
